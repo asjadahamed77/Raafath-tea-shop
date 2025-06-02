ff5e445ea372569730864697a6c33832f17a85a5
asjadahamed77 <ahamedasjad77@gmail.com>
2025-06-02 12:51:47 +0530

    Completed Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Account from "./pages/Account"
 import Box from "./pages/Box"
+import Login from "./pages/Login"
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
       <Route path="contact" element ={ <Contact />} />
       <Route path="account" element ={ <Account />} />
       <Route path="box" element ={ <Box />} />
+      <Route path="login" element ={ <Login />} />
 
       </Route>
     </Routes>
