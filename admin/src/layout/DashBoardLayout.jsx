02246da05a49ddefc355ff0c7ddcce72f301b17e
asjadahamed77 <ahamedasjad77@gmail.com>
2025-06-04 23:06:38 +0530

    Completed Navbar Responsively

--- admin/src/layout/DashBoardLayout.jsx
+++ admin/src/layout/DashBoardLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const DashBoardLayout = () => {
   return (
@@ -9,9 +10,12 @@ const DashBoardLayout = () => {
       <div className="hidden md:block fixed top-0 left-0  h-full p-[15px] overflow-y-auto ">
         <Sidebar />
       </div>
+      <div className='block md:hidden fixed top-0 left-0 right-0'>
+        <Navbar />
+      </div>
 
       {/* Main Content */}
-      <div className="flex-grow md:ml-[300px] w-full h-full overflow-y-auto py-[30px] px-[10px] md:px-[30px]">
+      <div className="flex-grow md:ml-[300px] w-full h-full overflow-y-auto md:py-[30px] px-[10px] md:px-[30px] py-20 pt-28 md:pt-[30px] ">
         <Outlet />
       </div>
     </div>
