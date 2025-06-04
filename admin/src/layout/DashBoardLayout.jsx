808a175dc264c18c105dbd743e2d901d460a5d0c
asjadahamed77 <ahamedasjad77@gmail.com>
2025-06-04 14:00:53 +0530

    Completed CakeItems page

--- admin/src/layout/DashBoardLayout.jsx
+++ admin/src/layout/DashBoardLayout.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const DashBoardLayout = () => {
   return (
-    <div className='flex '>
-      <div className='w-[280px] p-[15px] h-screen'>
+    <div className="flex h-screen overflow-hidden">
+      {/* Fixed Sidebar (desktop only) */}
+      <div className="hidden md:block fixed top-0 left-0  h-full p-[15px] overflow-y-auto ">
         <Sidebar />
       </div>
-      <div className='flex-grow p-[15px] h-screen overflow-y-auto'>
+
+      {/* Main Content */}
+      <div className="flex-grow md:ml-[300px] w-full h-full overflow-y-auto py-[30px] px-[10px] md:px-[30px]">
         <Outlet />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashBoardLayout
+export default DashBoardLayout;
